73a0426864b8e7293ef9d80a5dcdb223a6e7fb36
Robert Christian <robertjchristian@gmail.com>
2013-08-05 09:50:49 -0700

    post larger body

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 while true; do
 
-  curl -X POST -d "foo" --header "Cookie:  token=12345678" localhost:8000
+  curl -X POST -d @test.payload --header "Cookie:  token=12345678" localhost:8000
 
   curl --header "Authorization:  Basic cm9vdDpzZWNyZXQ=" localhost:8000
 
