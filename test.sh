e503d15fe4e84b476aff5cfd8a2d3085ad32a0a4
Robert Christian <robertjchristian@gmail.com>
2013-08-06 23:04:26 -0700

    move tests

--- test.sh
+++
@@ -1,9 +0,0 @@
-while true; do
-
-  curl -X POST -d @test.payload --header "Cookie:  token=12345678" localhost:8000
-
-  curl --header "Authorization:  Basic cm9vdDpzZWNyZXQ=" localhost:8000
-
-  curl --header "Authorization:  Basic cm9iOnJvYg==" localhost:8000
-
-done
